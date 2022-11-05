45e17b87a77a3601198510688cb7b6a06432822b
mtseng15 <tseng.micah@gmail.com>
2022-11-05 10:26:32 -0500

    added pll2022 to repo

--- init_repo.sh
+++ init_repo.sh
@@ -7,9 +7,11 @@ git submodule update --init
 
 # sparse checkout the mag
 cd ./mag/sky130-10-bit-SAR-ADC; git sparse-checkout init --cone; git sparse-checkout set mag; cd ../..
+cd ./mag/pll2022; git sparse-checkout init --cone; git sparse-checkout set mag; cd ../..
 
 # sparse checkout the xschem
 cd ./xschem/sky130-10-bit-SAR-ADC; git sparse-checkout init --cone; git sparse-checkout set xschem; cd ../..
+cd ./xschem/pll2022; git sparse-checkout init --cone; git sparse-checkout set xschem; cd ../..
 
 
 
