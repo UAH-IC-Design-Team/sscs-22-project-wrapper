f84f2ddc4cc6bb59266d670b372db1828fa03a40
mtseng15 <tseng.micah@gmail.com>
2022-11-05 09:51:49 -0500

    added helpers

---
+++ init_repo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Initialize the repo with sparse checkouts
+
+# Initialize the submodules
+git submodule update --init
+
+# sparse checkout the mag
+cd ./mag/sky130-10-bit-SAR-ADC; git sparse-checkout init --cone; git sparse-checkout set mag; cd ../..
+
+# sparse checkout the xschem
+cd ./xschem/sky130-10-bit-SAR-ADC; git sparse-checkout init --cone; git sparse-checkout set xschem; cd ../..
+
+
+
