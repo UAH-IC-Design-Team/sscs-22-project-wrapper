2e3b6bc8aa4d8d60eb0c48e0b0b940ea00db2c1d
mtseng15 <tseng.micah@gmail.com>
2022-11-12 18:15:19 -0600

    updated submodules and created a compression makefile

--- init_repo.sh
+++ init_repo.sh
@@ -12,6 +12,8 @@ cd ./mag/pll2022; git sparse-checkout init --cone; git sparse-checkout set mag; 
 # sparse checkout the xschem
 cd ./xschem/sky130-10-bit-SAR-ADC; git sparse-checkout init --cone; git sparse-checkout set xschem; cd ../..
 cd ./xschem/pll2022; git sparse-checkout init --cone; git sparse-checkout set xschem; cd ../..
+cd ./xschem/SSCS_PICO_tapeout; git sparse-checkout init --cone; git sparse-checkout set LNA_Vband/xschem Switch_tapeout/xschem VGA_tapeout/xschem; cd ../..
 
-
-
+# sparse checkout the gds
+cd ./gds/sky130-10-bit-SAR-ADC; git sparse-checkout init --cone; git sparse-checkout set gds; cd ../..
+cd ./gds/SSCS_PICO_tapeout; git sparse-checkout init --cone; git sparse-checkout set BALUN_VBAND/gds LNA_Vband/gds Standalone_cells/gds Switch_tapeout/gds VGA_tapeout/gds ; cd ../..
